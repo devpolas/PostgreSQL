04f4fcf9d49f556180378a1eb6e7f77fced8a96c
devpolas <195308698+devpolas@users.noreply.github.com>
2025-08-23 13:58:37 +0600

    finished day 1

--- phoneDatabase/queryData.sql
+++ phoneDatabase/queryData.sql
@@ -6,10 +6,37 @@
 --   price*units_sold AS revenue
 -- FROM
 --   phones;
+-- -----------------------------------------
+-- SELECT
+--   name,
+--   price
+-- FROM
+--   phones
+-- WHERE
+--   units_sold>5000;
+-- -----------------------------------------
+-- SELECT
+--   name,
+--   manufacturer
+-- FROM
+--   phones
+-- WHERE
+--   manufacturer IN ('Apple', 'Samsung');
+-- ------------------------------------------
+-- SELECT
+--   name,
+--   manufacturer
+-- FROM
+--   phones
+-- WHERE
+--   manufacturer='Apple'
+--   OR manufacturer='Samsung';
+-- --------------------------------------------
 SELECT
   name,
-  price
+  price,
+  (price*units_sold) AS total_revenue
 FROM
   phones
 WHERE
-  units_sold>5000;
+  (price*units_sold)>1000000;
